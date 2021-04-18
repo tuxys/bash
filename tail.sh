a90147356b5e52e2011a4a279dcf2a49ee54e769
Tux <77721526+tuxys@users.noreply.github.com>
2021-04-18 11:14:56 -0600

    Update tail.sh
    Fixed some tabs :)

--- tail.sh
+++ tail.sh
@@ -31,53 +31,53 @@ tail -f $log | while read line; do
     *) upg='' ;;
   esac #search
 
-	## HTTP-Codes Monitor
-	case "$ucd" in
-		200) hcd='GOOD' ;;
-		301) hcd='MOVE' ;;
-		302) hcd='MOVE' ;;
-		400) hcd='DENY' ;;
-		401) hcd='DENY' ;;
-		403) hcd='DENY' ;;
-		404) hcd='NONE' ;;
-		405) hcd='DENY' ;;
-		444) hcd='DENY' ;;
-		429) hcd='FAST' ;;
-		500) hcd='IERR' ;;
-		502) hcd='BERR' ;;
-		504) hcd='TOUT' ;;
-		*) hcd='UNKW' ;;
-	esac #codes
+  ## HTTP-Codes Monitor
+  case "$ucd" in
+    200) hcd='GOOD' ;;
+    301) hcd='MOVE' ;;
+    302) hcd='MOVE' ;;
+    400) hcd='DENY' ;;
+    401) hcd='DENY' ;;
+    403) hcd='DENY' ;;
+    404) hcd='NONE' ;;
+    405) hcd='DENY' ;;
+    444) hcd='DENY' ;;
+    429) hcd='FAST' ;;
+    500) hcd='IERR' ;;
+    502) hcd='BERR' ;;
+    504) hcd='TOUT' ;;
+    *) hcd='UNKW' ;;
+  esac #codes
 
-	## Search Engines Monitor
-	case "$uag" in
-		*Slurp*) uag='Yahoo' ;;
-		*bingbot*) uag='Bing' ;;
-		*Googlebot*) uag='Google' ;;
-		*YandexBot*) uag='Yandex' ;;
-		*DuckDuckBot*) uag='DuckGo' ;;
-		*Googlebot-Mobile*) uag='GoMobile' ;;
-		*Mediapartners-Google*) uag='AdGoogle' ;;
-		*Google-AdSense-Check*) uag='GoAdCheck' ;;
-	esac ##engines
+  ## Search Engines Monitor
+  case "$uag" in
+    *Slurp*) uag='Yahoo' ;;
+    *bingbot*) uag='Bing' ;;
+    *Googlebot*) uag='Google' ;;
+    *YandexBot*) uag='Yandex' ;;
+    *DuckDuckBot*) uag='DuckGo' ;;
+    *Googlebot-Mobile*) uag='GoMobile' ;;
+    *Mediapartners-Google*) uag='AdGoogle' ;;
+    *Google-AdSense-Check*) uag='GoAdCheck' ;;
+  esac ##engines
 
   ## Fixing Some Text
-	urf=${urf//\"/}
-	uag=${uag/;/}
-	upg=${upg//\%20/ }
+  urf=${urf//\"/}
+  uag=${uag/;/}
+  upg=${upg//\%20/ }
 
   ## Styling Some More Colors
-	uip="\e[38;5;66m$uip\e[0m"
-	hcd="\e[38;5;65m[$hcd]\e[0m"
-	uag="\e[38;5;243m$uag\e[0m"
+  uip="\e[38;5;66m$uip\e[0m"
+  hcd="\e[38;5;65m[$hcd]\e[0m"
+  uag="\e[38;5;243m$uag\e[0m"
 
-	if [ "$txt" ]; then
-		echo -e " $txt \n $hcd $uip \n"
-	fi #pages
+  if [ "$txt" ]; then
+    echo -e " $txt \n $hcd $uip \n"
+  fi #pages
 
-	if [ "$upg" ]; then
-		echo -e " $upg \n $hcd $uip \n"
-	fi #search
+  if [ "$upg" ]; then
+    echo -e " $upg \n $hcd $uip \n"
+  fi #search
 
 done #while
 
